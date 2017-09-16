660746426d76148136aa76a0811d6e264faace9e
Nick Cotter <nick@extropy.net>
2017-09-16 17:54:54 +0100

    first version, issues with legend

---
+++ plot4.R
@@ -0,0 +1,28 @@
+if(!file.exists("household_power_consumption.txt")) { 
+        message("Data file not present, downloading and unzipping...");
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+                      "household_power_consumption.zip")
+        unzip("household_power_consumption.zip")
+}
+
+message("loading data into hpc data frame")
+
+hpc <- read.csv("household_power_consumption.txt", sep = ";", na.strings = c("?"))
+
+message("loading feb 1st & 2nd 2007 data into hpc_feb data frame")
+
+hpc$Date <- as.Date(hpc$Date, format("%d/%m/%Y"))
+hpc_feb <- subset(hpc, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+hpc_feb$DateTime <- as.POSIXct(paste(hpc_feb$Date, hpc_feb$Time))
+
+par(mfcol=c(2,2))
+plot(Global_active_power ~ DateTime, data = hpc_feb, type="l", ylab = "Global Active Power", xlab="")
+
+plot(Sub_metering_1 ~ DateTime, data = hpc_feb, type="l", ylab = "Energy sub metering", xlab="")
+points(Sub_metering_2 ~ DateTime, data = hpc_feb, type="l", col="red")
+points(Sub_metering_3 ~ DateTime, data = hpc_feb, type="l", col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, bty = "n")
+
+plot(Voltage ~ DateTime, data = hpc_feb, type="l", xlab="datetime")
+
+plot(Global_reactive_power ~ DateTime, data = hpc_feb, type="l", xlab="datetime")
